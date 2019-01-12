710cd6bb81dc58f7e225ccf800e028a0ea610c35
Hadrian Buckner <bladeastro@gmail.com>
2019-01-12 16:11:57 -0500

    Update twitterproj.py
    Reformatted. Edited makeEven to take in a UserID to make it more usable. Reformatted error handling to actually tell whats wrong.

--- twitterproj.py
+++ twitterproj.py
@@ -1,17 +1,18 @@
 import twitter
 import time
 from req import *
+import datetime
 
 api = twitter.Api(consumer_key=a,consumer_secret=b,access_token_key=c,access_token_secret=d,sleep_on_rate_limit=True)
 api.VerifyCredentials()
 '''
-Description: searches term then follows users
+Description: searches term then adds users to list
 input: String
 @returns: List of ids
 '''
 def getlist(searchterm):
     results = api.GetSearch(
-    term=searchterm,count=100)
+    term=searchterm,count=100,result_type="recent")
     idlist=[]
     idlist=[u.user for u in results]
     newlist=[u.screen_name.encode("utf-8") for u in idlist]
@@ -30,41 +31,31 @@ def followbot(List):
 '''
 Description:Checks to see if user is freinds with x if not unfollows user.
 input:list, user_id
+returns: time that username and time the user was unfollowed in a list
 '''
 def checkifFriend(List,user_id):
+    returnlist=[]
+    userdelete=[]
     for x in List:
         #   print(api.ShowFriendship(source_user_id=173834439,target_screen_name=x)['relationship']['target']['following'])
         if api.ShowFriendship(user_id,target_screen_name=x)['relationship']['target']['following'] == False:
             try:
                 api.DestroyFriendship(screen_name=x)
+                userdelete=[x,datetime.datetime.now()]
+                returnlist.append(userdelete)
                 print("friendship with "+x+" is being destroyed")
             except:
-                print("there was an issue with ")
-
+                raise
+                break
+    return userdelete
 '''
 Description: gets friends of user checks to see if friends if not, unfollows them
 '''
-def makeEven():
+def makeEven(UserID):
     try:
         users=api.GetFriends()
         friendids=[u.screen_name for u in users]
-        checkifFriend(friendids,"173834439")
+        checkifFriend(friendids,UserID)
     except:
-        print("there is issue in makeEven")
-
-
-def main():
-    #search name
-    while(True):
-        searching=getlist("Developer")
-    #follow who tweets about search term
-        followbot(searching)
-        print("following")
-        print(searching)
-    #wait 1 hour
-        #time.sleep(600)
-        checkifFriend(searching)
-    #check to see if they followed back if not unfoll)ow
-
-makeEven()
-#main()
+        raise
+    
